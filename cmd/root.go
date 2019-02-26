4b8b79f8bca880531bd61b5ac17120974f1dc87b
drzero42 <anders.olsen@cellpointmobile.com>
2019-02-26 16:53:42 +0100

    Bump to version 0.1.2.

--- cmd/root.go
+++ cmd/root.go
@@ -37,7 +37,7 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Version: "v0.1.1",
+	Version: "v0.1.2",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
