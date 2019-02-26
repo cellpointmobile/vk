d8860784b9c19055b3d22b4bb3e51c8a85717145
drzero42 <anders.olsen@cellpointmobile.com>
2019-02-26 13:34:30 +0100

    Load definitions from external json file.

--- programs/programs.go
+++ programs/programs.go
@@ -15,194 +15,74 @@
 package programs
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/drzero42/vk/program"
+	"github.com/gregjones/httpcache"
+	"github.com/gregjones/httpcache/diskcache"
+	"github.com/tidwall/gjson"
 )
 
 // LoadPrograms returns a map of programs
 func LoadPrograms(bindir string) map[string]program.IProgram {
 	path := os.ExpandEnv(bindir)
-	Progs := make(map[string]program.IProgram)
+	cacheclient := httpcache.NewTransport(diskcache.New(os.ExpandEnv("$HOME/.vk/definitions-cache"))).Client()
+	resp, err := cacheclient.Get("https://drzero42.github.io/vk-definitions/vk-definitions.json")
+	if err != nil {
+		panic(fmt.Sprintf("Could not download definitions: %s\n", err))
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(fmt.Sprintf("Could not download definitions: %s\n", err))
+	}
 
-	Progs["ark"] = program.NewGithubDownloadUntarFileProgram(
-		"ark",
-		path,
-		"version",
-		"Version: (v.+?)\n",
-		"heptio",
-		"velero",
-		"ark-{VVERSION}-linux-amd64.tar.gz",
-		false,
-		"",
-		"ark",
-	)
+	directdownload := gjson.GetBytes(body, "github.directdownload")
+	untarfile := gjson.GetBytes(body, "github.untarfile")
+	unzipfile := gjson.GetBytes(body, "github.unzipfile")
+	hashicorp := gjson.GetBytes(body, "hashicorp")
 
-	Progs["dive"] = program.NewGithubDownloadUntarFileProgram(
-		"dive",
-		path,
-		"version",
-		"dive (.+?)\n",
-		"wagoodman",
-		"dive",
-		"dive_{VERSION}_linux_amd64.tar.gz",
-		true,
-		"",
-		"dive",
-	)
+	progs := make(map[string]program.IProgram)
 
-	Progs["docker-compose"] = program.NewGithubDirectDownloadProgram(
-		"docker-compose",
-		path,
-		"version",
-		"docker-compose version (.+?),",
-		"docker",
-		"compose",
-		"docker-compose-Linux-x86_64",
-		false,
-		"",
-	)
+	for _, v := range directdownload.Array() {
+		var prog program.GithubDirectDownloadProgram
+		err := json.Unmarshal([]byte(v.String()), &prog)
+		if err != nil {
+			panic(err)
+		}
+		prog.Command.Path = path
+		progs[prog.Command.Cmd] = &prog
+	}
+	for _, v := range untarfile.Array() {
+		var prog program.GithubDownloadUntarFileProgram
+		err := json.Unmarshal([]byte(v.String()), &prog)
+		if err != nil {
+			panic(err)
+		}
+		prog.Command.Path = path
+		progs[prog.Command.Cmd] = &prog
+	}
+	for _, v := range unzipfile.Array() {
+		var prog program.GithubDownloadUnzipFileProgram
+		err := json.Unmarshal([]byte(v.String()), &prog)
+		if err != nil {
+			panic(err)
+		}
+		prog.Command.Path = path
+		progs[prog.Command.Cmd] = &prog
+	}
+	for _, v := range hashicorp.Array() {
+		var prog program.HashicorpProgram
+		err := json.Unmarshal([]byte(v.String()), &prog)
+		if err != nil {
+			panic(err)
+		}
+		prog.Command.Path = path
+		progs[prog.Command.Cmd] = &prog
+	}
 
-	Progs["fly"] = program.NewGithubDirectDownloadProgram(
-		"fly",
-		path,
-		"--version",
-		"(.+?)\n",
-		"concourse",
-		"concourse",
-		"fly_linux_amd64",
-		false,
-		"",
-	)
-
-	Progs["helm"] = program.NewGithubDownloadUntarFileProgram(
-		"helm",
-		path,
-		"version --client",
-		"SemVer:\"(.+?)\"",
-		"kubernetes",
-		"helm",
-		"",
-		false,
-		"https://storage.googleapis.com/kubernetes-helm/helm-{VVERSION}-linux-amd64.tar.gz",
-		"linux-amd64/helm",
-	)
-
-	Progs["helmfile"] = program.NewGithubDirectDownloadProgram(
-		"helmfile",
-		path,
-		"--version",
-		"version (v.+?)\n",
-		"roboll",
-		"helmfile",
-		"helmfile_linux_amd64",
-		false,
-		"",
-	)
-
-	Progs["img"] = program.NewGithubDirectDownloadProgram(
-		"img",
-		path,
-		"version",
-		"version.+?(v.+?)\n",
-		"genuinetools",
-		"img",
-		"img-linux-amd64",
-		false,
-		"",
-	)
-
-	Progs["k8sec"] = program.NewGithubDownloadUntarFileProgram(
-		"k8sec",
-		path,
-		"version",
-		"version (v.+?),",
-		"dtan4",
-		"k8sec",
-		"k8sec-{VVERSION}-linux-amd64.tar.gz",
-		false,
-		"",
-		"linux-amd64/k8sec",
-	)
-
-	Progs["kops"] = program.NewGithubDirectDownloadProgram(
-		"kops",
-		path,
-		"version",
-		"Version (.+?) ",
-		"kubernetes",
-		"kops",
-		"kops-linux-amd64",
-		false,
-		"",
-	)
-
-	Progs["kustomize"] = program.NewGithubDirectDownloadProgram(
-		"kustomize",
-		path,
-		"version",
-		"KustomizeVersion:(.+?) ",
-		"kubernetes-sigs",
-		"kustomize",
-		"kustomize_{VVERSION}_linux_amd64",
-		false,
-		"",
-	)
-
-	Progs["minikube"] = program.NewGithubDirectDownloadProgram(
-		"minikube",
-		path,
-		"version",
-		"version: (v.+?)\n",
-		"kubernetes",
-		"minikube",
-		"minikube-linux-amd64",
-		false,
-		"",
-	)
-
-	Progs["stern"] = program.NewGithubDirectDownloadProgram(
-		"stern",
-		path,
-		"--version",
-		"version (.+?)\n",
-		"wercker",
-		"stern",
-		"stern_linux_amd64",
-		false,
-		"",
-	)
-
-	Progs["terraform"] = program.NewHashicorpProgram(
-		"terraform",
-		path,
-		"Terraform (v.+?)\n",
-	)
-
-	Progs["terraform-docs"] = program.NewGithubDirectDownloadProgram(
-		"terraform-docs",
-		path,
-		"--version",
-		"(.+?)\n",
-		"segmentio",
-		"terraform-docs",
-		"terraform-docs-{VVERSION}-linux-amd64",
-		false,
-		"",
-	)
-
-	Progs["tflint"] = program.NewGithubDownloadUnzipFileProgram(
-		"tflint",
-		path,
-		"--version",
-		"version (.+?)\n",
-		"wata727",
-		"tflint",
-		"tflint_linux_amd64.zip",
-		false,
-		"",
-		"tflint",
-	)
-
-	return Progs
+	return progs
 }
