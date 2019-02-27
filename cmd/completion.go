a474b0d166e7708e7354149174397cae7013a9fe
Anders Bruun Olsen <anders.olsen@cellpointmobile.com>
2019-02-27 11:38:07 +0100

    Add initial completion support.

---
+++ cmd/completion.go
@@ -0,0 +1,60 @@
+// Copyright © 2019 Cellpoint Mobile
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// completionCmd represents the completion command
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generate shell completion scripts",
+	Long: `To load completion run
+	
+source <(vk completion [bash|zsh])
+
+To configure your bash shell to load completions for each session add to your bashrc
+
+# ~/.bashrc or ~/.profile
+. <(vk completion bash)
+`,
+	ValidArgs: []string{"bash", "zsh"},
+	// Switch to cobra.ExactValidArgs(1) when new release of Cobra happens.
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if args[0] == "bash" {
+			rootCmd.GenBashCompletion(os.Stdout)
+		} else if args[0] == "zsh" {
+			rootCmd.GenZshCompletion(os.Stdout)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// completionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// completionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
