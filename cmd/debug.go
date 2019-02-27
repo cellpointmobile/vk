d6cacd839b4e3bff4f0c4b9ef99216daefa71a97
Anders Bruun Olsen <anders.olsen@cellpointmobile.com>
2019-02-27 14:40:58 +0100

    Allow debug command to iterate over all definitions.

--- cmd/debug.go
+++ cmd/debug.go
@@ -18,46 +18,65 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sort"
+
+	"github.com/cellpointmobile/vk/program"
 
 	"github.com/cellpointmobile/vk/programs"
 	"github.com/spf13/cobra"
 )
+
+func debugProgram(p program.IProgram) {
+	fmt.Printf("Debugging tool %s\n", p.GetCmd())
+	fmt.Printf("Struct: %#v\n", p)
+	isInstalled := p.IsInstalled()
+	fmt.Printf("Is installed: %t\n", isInstalled)
+	if isInstalled {
+		fmt.Printf("Local version: %s\n", p.GetLocalVersion())
+	}
+	v, err := p.GetLatestVersion()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Can't get latest version.")
+		os.Exit(10)
+	}
+	fmt.Printf("Latest version: %s\n", v)
+	url := p.GetLatestDownloadURL()
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Something went wrong with the HTTP client: %s", err)
+		os.Exit(20)
+	}
+	if resp.StatusCode == 200 {
+		fmt.Printf("Download URL: %s\n", url)
+	} else {
+		fmt.Printf("Invalid DownloadURL: %s (Status code %d)", url, resp.StatusCode)
+	}
+}
 
 // debugCmd represents the debug command
 var debugCmd = &cobra.Command{
 	Use:   "debug",
 	Short: "Debug a tool definition",
 	Long:  `This subcommand debugs a tool definition.`,
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		progname := args[0]
 		progs := programs.LoadPrograms(cmd.Flag("bindir").Value.String())
-		if prog, ok := progs[progname]; ok {
-			fmt.Printf("Struct: %#v\n", prog)
-			isInstalled := prog.IsInstalled()
-			fmt.Printf("Is installed: %t\n", isInstalled)
-			if isInstalled {
-				fmt.Printf("Local version: %s\n", prog.GetLocalVersion())
+		if len(args) == 0 {
+			keys := make([]string, 0, len(progs))
+			for k := range progs {
+				keys = append(keys, k)
 			}
-			v, err := prog.GetLatestVersion()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Can't get latest version.")
-				os.Exit(10)
-			}
-			fmt.Printf("Latest version: %s\n", v)
-			url := prog.GetLatestDownloadURL()
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Something went wrong with the HTTP client: %s", err)
-				os.Exit(20)
-			}
-			if resp.StatusCode == 200 {
-				fmt.Printf("Download URL: %s\n", url)
-			} else {
-				fmt.Printf("Invalid DownloadURL: %s (Status code %d)", url, resp.StatusCode)
+			sort.Strings(keys)
+			for _, k := range keys {
+				debugProgram(progs[k])
 			}
 		} else {
-			fmt.Printf("Unknown program: %s\n", progname)
+			progname := args[0]
+			if prog, ok := progs[progname]; ok {
+				debugProgram(prog)
+			} else {
+				fmt.Fprintf(os.Stderr, "Unknown program: %s\n", progname)
+			}
 		}
 	},
 }
